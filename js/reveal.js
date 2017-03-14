ee33b8b0272fd781b74b126f39268998cc307e13
zmarhesta <mzubeirarhesta@gmail.com>
2017-03-14 07:39:24 +0700

    updating

--- js/reveal.js
+++ js/reveal.js
@@ -56,7 +56,7 @@
 			progress: true,
 
 			// Display the page number of the current slide
-			slideNumber: false,
+			slideNumber: true,
 
 			// Push each slide change to the browser history
 			history: false,
@@ -135,7 +135,7 @@
 			focusBodyOnPageVisibilityChange: true,
 
 			// Transition style
-			transition: 'slide', // none/fade/slide/convex/concave/zoom
+			transition: 'concave', // none/fade/slide/convex/concave/zoom
 
 			// Transition speed
 			transitionSpeed: 'default', // default/fast/slow
